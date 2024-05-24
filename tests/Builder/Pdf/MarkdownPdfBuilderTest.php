6f7ca78f7cf16580da81da8f1b06e7e3614793ec
Adrien Roches <adrien@roc-it.tech>
2024-05-24 08:42:57 +0200

    [Fix] Namespace

--- tests/Builder/Pdf/MarkdownPdfBuilderTest.php
+++ tests/Builder/Pdf/MarkdownPdfBuilderTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Sensiolabs\GotenbergBundle\Tests\Builder\pdf;
+namespace Sensiolabs\GotenbergBundle\Tests\Builder\Pdf;
 
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\UsesClass;
