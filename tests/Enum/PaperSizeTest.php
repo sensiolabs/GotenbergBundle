f7cab976a05b67f057c19beb4eae8c6d2aa226da
Adrien Roches <adrien@roc-it.tech>
2024-05-30 17:53:11 +0200

    [UPDATE] Add unit of enum PaperSize to tests

--- tests/Enum/PaperSizeTest.php
+++ tests/Enum/PaperSizeTest.php
@@ -5,10 +5,18 @@ namespace Sensiolabs\GotenbergBundle\Tests\Enum;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use Sensiolabs\GotenbergBundle\Enum\PaperSize;
+use Sensiolabs\GotenbergBundle\Enum\Unit;
 
 #[CoversClass(PaperSize::class)]
 final class PaperSizeTest extends TestCase
 {
+    public function testUnitIsAlwaysInches(): void
+    {
+        foreach (PaperSize::cases() as $size) {
+            self::assertSame(Unit::Inches, $size->unit());
+        }
+    }
+
     public function testWidth(): void
     {
         foreach (PaperSize::cases() as $size) {
