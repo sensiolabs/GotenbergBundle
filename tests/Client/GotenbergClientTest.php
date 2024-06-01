0499e5c747419f0d2c0832297491815ca5090a23
Adrien Roches <adrien@roc-it.tech>
2024-06-01 14:41:48 +0200

    [UPDATE] Change the GotenbergClientTest

--- tests/Client/GotenbergClientTest.php
+++ tests/Client/GotenbergClientTest.php
@@ -9,17 +9,13 @@ use Sensiolabs\GotenbergBundle\Client\GotenbergClient;
 use Sensiolabs\GotenbergBundle\Client\GotenbergResponse;
 use Symfony\Component\HttpClient\MockHttpClient;
 use Symfony\Component\HttpClient\Response\MockResponse;
-use Symfony\Component\HttpFoundation\HeaderBag;
 use Symfony\Component\HttpFoundation\Response;
 
 #[CoversClass(GotenbergClient::class)]
 #[UsesClass(GotenbergResponse::class)]
-#[UsesClass(MockResponse::class)]
-#[UsesClass(MockHttpClient::class)]
-#[UsesClass(HeaderBag::class)]
 final class GotenbergClientTest extends TestCase
 {
-    public function testPdfCall(): void
+    public function testCallIsCorrectlyFormatted(): void
     {
         /** @var string $stream */
         $stream = file_get_contents(__DIR__.'/../Fixtures/pdf/simple_pdf.pdf');
@@ -34,32 +30,55 @@ final class GotenbergClientTest extends TestCase
 
         $mockClient = new MockHttpClient([$mockResponse]);
 
+        $multipartFormData = [
+            [
+                'url' => 'https://google.com',
+            ],
+        ];
+
         $gotenbergClient = new GotenbergClient('http://localhost:3000', $mockClient);
-        $response = $gotenbergClient->call('/forms/chromium/convert/url', []);
+        $response = $gotenbergClient->call('/some/url', $multipartFormData, ['SomeHeader' => 'SomeValue']);
+
+        self::assertSame(1, $mockClient->getRequestsCount());
+        self::assertSame('POST', $mockResponse->getRequestMethod());
+        self::assertSame('http://localhost:3000/some/url', $mockResponse->getRequestUrl());
+
+        $requestHeaders = array_reduce($mockResponse->getRequestOptions()['headers'], function (array $carry, string $header): array {
+            /** @see https://onlinephp.io/c/8de64 */
+            $matches = null;
+            preg_match('#^(?P<key>[^:]+):\s?(?P<value>.*)$#', $header, $matches);
+
+            [
+                'key' => $key,
+                'value' => $value,
+            ] = $matches;
+
+            $carry[$key] ??= [];
+            $carry[$key][] = $value;
+
+            return $carry;
+        }, []);
+
+        self::assertArrayHasKey('SomeHeader', $requestHeaders);
+        self::assertSame('SomeValue', $requestHeaders['SomeHeader'][0]);
+
+        self::assertArrayHasKey('content-type', $requestHeaders);
+        $requestContentType = $requestHeaders['content-type'][0];
+
+        self::assertMatchesRegularExpression('#^multipart/form-data; boundary=(?P<boundary>.*)$#', $requestContentType);
+
+        /** @see https://onlinephp.io/c/e8233 */
+        $matches = null;
+        preg_match('#^multipart/form-data; boundary=(?P<boundary>.*)$#', $requestContentType, $matches);
+        $boundary = $matches['boundary'];
+
+        $requestBody = $mockResponse->getRequestOptions()['body'];
+        self::assertSame(
+            "--{$boundary}\r\nContent-Type: text/plain; charset=utf-8\r\nContent-Transfer-Encoding: 8bit\r\nContent-Disposition: form-data; name=\"url\"\r\n\r\nhttps://google.com\r\n--{$boundary}--\r\n",
+            $requestBody,
+        );
 
         self::assertSame(Response::HTTP_OK, $response->getStatusCode());
         self::assertSame('application/pdf', $response->headers->get('content-type'));
-    }
-
-    public function testScreenshotCall(): void
-    {
-        /** @var string $stream */
-        $stream = file_get_contents(__DIR__.'/../Fixtures/screenshot/SensioLabs.png');
-        $mockResponse = new MockResponse($stream, [
-            'http_code' => Response::HTTP_OK,
-            'response_headers' => [
-                'accept-ranges' => 'bytes',
-                'content-disposition' => 'attachment; filename="SensioLabs.png"',
-                'content-type' => 'image/png',
-            ],
-        ]);
-
-        $mockClient = new MockHttpClient([$mockResponse]);
-
-        $gotenbergClient = new GotenbergClient('http://localhost:3000', $mockClient);
-        $response = $gotenbergClient->call('/forms/chromium/screenshot/url', []);
-
-        self::assertSame(Response::HTTP_OK, $response->getStatusCode());
-        self::assertSame('image/png', $response->headers->get('content-type'));
     }
 }
