c32fe09cb76441d08be2b10346cfb75a39c19c61
Adrien Roches <adrien@roc-it.tech>
2024-06-21 14:23:46 +0200

    [UPDATE] stuff

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -2,7 +2,14 @@
 
 namespace PHPSTORM_META {
     expectedArguments(
-        \Sensiolabs\GotenbergBundle\Builder\AbstractBuilder::fileName(),
+        \Sensiolabs\GotenbergBundle\Builder\Pdf\AbstractPdfBuilder::fileName(),
+        1,
+        \Symfony\Component\HttpFoundation\HeaderUtils::DISPOSITION_INLINE,
+        \Symfony\Component\HttpFoundation\HeaderUtils::DISPOSITION_ATTACHMENT,
+    );
+
+    expectedArguments(
+        \Sensiolabs\GotenbergBundle\Builder\Screenshot\AbstractScreenshotBuilder::fileName(),
         1,
         \Symfony\Component\HttpFoundation\HeaderUtils::DISPOSITION_INLINE,
         \Symfony\Component\HttpFoundation\HeaderUtils::DISPOSITION_ATTACHMENT,
