63c2ce55d322c50426deaa6bd509d261d1efc221
Adrien Roches <adrien@roc-it.tech>
2025-01-13 01:04:27 +0100

    Fix composer dependency scanning

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -13,6 +13,12 @@ return $config
     ->addPathToScan(__DIR__.'/src/Debug', isDev: true)
     ->addPathToScan(__DIR__.'/tests', isDev: true)
 
+    ->ignoreErrorsOnPackage('async-aws/s3', [
+        ErrorType::DEV_DEPENDENCY_IN_PROD,
+    ])
+    ->ignoreErrorsOnPackage('league/flysystem', [
+        ErrorType::DEV_DEPENDENCY_IN_PROD,
+    ])
     ->ignoreErrorsOnPackage('symfony/routing', [
         ErrorType::DEV_DEPENDENCY_IN_PROD,
     ])
