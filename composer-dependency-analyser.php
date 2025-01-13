5e74cfeb3604e42713bd421f8fb19d6eedd2fbcb
Adrien Roches <adrien@roc-it.tech>
2025-01-13 01:22:56 +0100

    Fix composer dependency analyser for ext-*

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -16,6 +16,7 @@ return $config
     ->ignoreErrorsOnPackage('async-aws/s3', [
         ErrorType::DEV_DEPENDENCY_IN_PROD,
     ])
+    ->disableExtensionsAnalysis() // TODO : Bug waiting for https://github.com/shipmonk-rnd/composer-dependency-analyser/issues/217
     ->ignoreErrorsOnPackage('league/flysystem', [
         ErrorType::DEV_DEPENDENCY_IN_PROD,
     ])
