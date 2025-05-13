429100639bf8f22e8deb752a5b1ec966b7ef931f
Adrien Roches <adrien@roc-it.tech>
2025-05-13 17:40:55 +0200

    Apply cs fixer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Symfony\Component\ErrorHandler\ErrorHandler;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require \dirname(__DIR__).'/vendor/autoload.php';
 
 // hack from https://github.com/symfony/symfony/issues/53812#issuecomment-1962740145
 // because of "Test code or tested code did not remove its own exception handlers"
