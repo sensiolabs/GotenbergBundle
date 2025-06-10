4ef2b19eb0019d391b50a5192c595d1a54c9ce0d
Hubert Lenoir <Jean-Beru@users.noreply.github.com>
2025-06-10 17:47:11 +0200

    Update src/Processor/FileProcessor.php
    Co-authored-by: Adrien Roches <Neirda24@users.noreply.github.com>

--- src/Processor/FileProcessor.php
+++ src/Processor/FileProcessor.php
@@ -38,7 +38,7 @@ final class FileProcessor implements ProcessorInterface
             $path = $this->directory.'/'.$fileName;
 
             $this->filesystem->dumpFile($path, $resource);
-            $this->logger?->debug('{processor}: content dumped to "{file}" on {path}.', ['processor' => self::class, 'file' => $fileName, 'path' => $path]);
+            $this->logger?->debug('{processor}: content dumped to "{path}".', ['processor' => self::class, 'path' => $path]);
 
             return new \SplFileInfo($path);
         } catch (\Throwable $t) {
