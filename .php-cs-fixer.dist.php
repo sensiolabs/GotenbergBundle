ab76b72d947aa21a3f9ec1a6cb642eb1cddd9bfc
Adrien Roches <adrien@roc-it.tech>
2025-07-05 16:17:01 +0200

    Fix cs-foxer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -12,6 +12,7 @@ $finder = (new PhpCsFixer\Finder())
 ;
 
 return (new PhpCsFixer\Config())
+    ->setUnsupportedPhpVersionAllowed(true)
     ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setUsingCache(false)
     ->setRiskyAllowed(true)
