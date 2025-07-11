82f947b756238a9ec35b211bcea4c13ac0c227da
Adrien Roches <adrien@roc-it.tech>
2025-07-11 14:30:11 +0200

    review

--- config/debug.php
+++ config/debug.php
@@ -36,7 +36,7 @@ return static function (ContainerConfigurator $container): void {
 
     $services->set('sensiolabs_gotenberg.data_collector', GotenbergDataCollector::class)
         ->args([
-            service('sensiolabs_gotenberg'),
+            service('sensiolabs_gotenberg.version_fetcher'),
             service('sensiolabs_gotenberg.pdf'),
             service('sensiolabs_gotenberg.screenshot'),
             tagged_locator('sensiolabs_gotenberg.builder'),
--- src/DataCollector/GotenbergDataCollector.php
+++ src/DataCollector/GotenbergDataCollector.php
@@ -7,7 +7,7 @@ use Sensiolabs\GotenbergBundle\Debug\Builder\TraceableBuilder;
 use Sensiolabs\GotenbergBundle\Debug\Client\TraceableGotenbergClient;
 use Sensiolabs\GotenbergBundle\Debug\TraceableGotenbergPdf;
 use Sensiolabs\GotenbergBundle\Debug\TraceableGotenbergScreenshot;
-use Sensiolabs\GotenbergBundle\GotenbergInterface;
+use Sensiolabs\GotenbergBundle\Version\VersionFetcherInterface;
 use Symfony\Component\DependencyInjection\ServiceLocator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,7 +24,7 @@ final class GotenbergDataCollector extends DataCollector implements LateDataColl
      * @param array<mixed>                                      $defaultOptions
      */
     public function __construct(
-        private readonly GotenbergInterface $gotenberg,
+        private readonly VersionFetcherInterface $versionFetcher,
         private readonly TraceableGotenbergPdf $traceableGotenbergPdf,
         private readonly TraceableGotenbergScreenshot $traceableGotenbergScreenshot,
         private readonly ServiceLocator $builders,
@@ -35,7 +35,7 @@ final class GotenbergDataCollector extends DataCollector implements LateDataColl
 
     public function collect(Request $request, Response $response, \Throwable|null $exception = null): void
     {
-        $this->data['version'] = (string) $this->gotenberg->version();
+        $this->data['version'] = (string) $this->versionFetcher->get();
         $this->data['request_total_size'] = 0;
         $this->data['request_total_time'] = 0;
         $this->data['request_count'] = 0;
