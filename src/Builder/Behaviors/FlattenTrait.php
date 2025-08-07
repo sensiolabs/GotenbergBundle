6f5027a473eaf76806b8e38d654dcc42902bd477
Adrien Roches <adrien@roc-it.tech>
2025-08-07 09:07:04 +0200

    Rollback builder bhavior

--- src/Builder/Behaviors/FlattenTrait.php
+++ src/Builder/Behaviors/FlattenTrait.php
@@ -4,11 +4,9 @@ namespace Sensiolabs\GotenbergBundle\Builder\Behaviors;
 
 use Sensiolabs\GotenbergBundle\Builder\Attributes\NormalizeGotenbergPayload;
 use Sensiolabs\GotenbergBundle\Builder\Attributes\WithConfigurationNode;
-use Sensiolabs\GotenbergBundle\Builder\Behaviors\Dependencies\LoggerAwareTrait;
 use Sensiolabs\GotenbergBundle\Builder\BodyBag;
 use Sensiolabs\GotenbergBundle\Builder\Util\NormalizerFactory;
 use Sensiolabs\GotenbergBundle\NodeBuilder\BooleanNodeBuilder;
-use Sensiolabs\GotenbergBundle\Version\Version;
 
 /**
  * @see https://gotenberg.dev/docs/routes#flatten-libreoffice
@@ -16,9 +14,6 @@ use Sensiolabs\GotenbergBundle\Version\Version;
 trait FlattenTrait
 {
     abstract protected function getBodyBag(): BodyBag;
-    abstract protected function getVersion(): Version;
-
-    use LoggerAwareTrait;
 
     /**
      * Flattening a PDF combines all its contents into a single layer. (default false).
@@ -26,10 +21,6 @@ trait FlattenTrait
     #[WithConfigurationNode(new BooleanNodeBuilder('flatten'))]
     public function flatten(bool $bool = true): self
     {
-        if ($this->getVersion()->isLowerThan('8.16.0')) {
-            $this->getLogger()?->warning('The flatten option is only available from version Gotenberg >= 8.16.0.');
-        }
-
         $this->getBodyBag()->set('flatten', $bool);
 
         return $this;
