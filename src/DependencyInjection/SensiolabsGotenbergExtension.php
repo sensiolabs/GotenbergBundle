c2d00032a0f99bc60ff2f131430bcbbceaa9b4fb
Adrien Roches <adrien@roc-it.tech>
2025-12-15 14:58:45 +0100

    chore(code): remove unnecessary 'if'

--- src/DependencyInjection/SensiolabsGotenbergExtension.php
+++ src/DependencyInjection/SensiolabsGotenbergExtension.php
@@ -166,11 +166,9 @@ class SensiolabsGotenbergExtension extends Extension
             }
         }
 
-        if ($container->hasDefinition('.sensiolabs_gotenberg.webhook_configuration_registry')) {
-            $registryDefinition = $container->getDefinition('.sensiolabs_gotenberg.webhook_configuration_registry');
-            foreach ($defaultConfiguration['webhook'] as $name => $webhookConfig) {
-                $registryDefinition->addMethodCall('add', [$name, $webhookConfig]);
-            }
+        $registryDefinition = $container->getDefinition('.sensiolabs_gotenberg.webhook_configuration_registry');
+        foreach ($defaultConfiguration['webhook'] as $name => $webhookConfig) {
+            $registryDefinition->addMethodCall('add', [$name, $webhookConfig]);
         }
 
         $container->getDefinition('sensiolabs_gotenberg.builder_configurator')
