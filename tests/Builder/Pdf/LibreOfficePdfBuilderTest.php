87a68b93c60f64927cfd4ade8d315695debf4c9c
jprivet-dev <jprivet.dev@gmail.com>
2026-04-27 14:46:22 +0200

    feat(LibreOfficePdfBuilderTest.php): create testZoomConfigurationRequirement()

--- tests/Builder/Pdf/LibreOfficePdfBuilderTest.php
+++ tests/Builder/Pdf/LibreOfficePdfBuilderTest.php
@@ -96,4 +96,16 @@ class LibreOfficePdfBuilderTest extends GotenbergBuilderTestCase
             ->generate()
         ;
     }
+
+    public function testZoomConfigurationRequirement(): void
+    {
+        $this->expectException(InvalidBuilderConfiguration::class);
+        $this->expectExceptionMessage('"zoom" can only be set when "magnification" is set to 4.');
+
+        $this->getBuilder()
+            ->files('assets/office/document.odt')
+            ->zoom(50)
+            ->generate()
+        ;
+    }
 }
